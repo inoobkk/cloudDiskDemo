5bfd3de8f692273349285e4d4e557fa457c9da21
inoobkk <1253707499@qq,com>
2021-07-23 09:16:54 +0800

    添加了QListWidgetItem* 成员

---
+++ tools/fileinfo.h
@@ -0,0 +1,12 @@
+﻿#ifndef FILEINFO_H
+#define FILEINFO_H
+#include <QString>
+#include <QListWidgetItem>
+class FileInfo{
+public:
+    QString fileName;   // 文件名
+    QString fileType;   // 文件类型
+    QListWidgetItem *item;
+};
+
+#endif // FILEINFO_H
