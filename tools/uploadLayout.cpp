5142235abd6d891827cbfdc663b8dd551f8530c8
inoobkk <1253707499@qq,com>
2021-07-25 21:23:25 +0800

    获取传输列表窗口的布局

---
+++ tools/uploadLayout.cpp
@@ -0,0 +1,23 @@
+﻿#include "uploadLayout.h"
+
+QLayout* UploadLayout::m_layout = NULL;
+QWidget* UploadLayout::m_wg = NULL;
+void UploadLayout::setUploadLayout(QWidget* p){
+    m_wg = new QWidget(p);
+    QLayout* layout = p->layout();
+    layout->addWidget(m_wg);
+    layout->setContentsMargins(0,0,0,0);        // 设置边距
+    QVBoxLayout* vlayout = new QVBoxLayout;     // 垂直布局
+    m_wg->setLayout(vlayout);
+    vlayout->setContentsMargins(0,0,0,0);
+    m_layout = vlayout;
+
+    // 添加弹簧
+    vlayout->addStretch();  // 添加一个弹簧
+
+
+}
+
+QLayout* UploadLayout::getUploadLayout(){
+    return m_layout;
+}
---
+++ tools/uploadLayout.h
@@ -0,0 +1,16 @@
+﻿#ifndef UPLOADLAYOUT_H
+#define UPLOADLAYOUT_H
+#include <QVBoxLayout>
+#include <QWidget>
+class UploadLayout{
+public:
+    static void setUploadLayout(QWidget* p);    // 设置布局
+    static QLayout* getUploadLayout();     // 获取布局
+private:
+    UploadLayout(){}
+    ~UploadLayout(){}
+    static QLayout* m_layout;
+    static QWidget* m_wg;
+};
+
+#endif // UPLOADLAYOUT_H
